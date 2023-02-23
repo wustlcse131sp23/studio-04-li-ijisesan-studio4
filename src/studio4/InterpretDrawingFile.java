275e10e8933543f756dfd22de2f38ff8bd26af2b
miaom <miaom@LAPTOP-NVIE6NS0.lan>
2023-02-23 13:38:30 -0600

    part 1

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,36 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean fill = in.nextBoolean();
+
+		StdDraw.setPenColor(red, green, blue);
+		if (shape.equals("rectangle")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double width = in.nextDouble();
+			double height = in.nextDouble();
+			if (fill) {
+				StdDraw.filledRectangle(x, y, width, height);
+			} else {
+				StdDraw.rectangle(x, y, width, height);
+			}
+		}
+		if (shape.equals("ellipse")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			//double a = in.nextDouble();
+			//double b = in.nextDouble();
+			double width = in.nextDouble();
+			double height = in.nextDouble();
+			if (fill) {
+				StdDraw.filledEllipse(x, y,  width, height);
+			} else {
+				StdDraw.ellipse(x, y, width, height);
+			}
+		}
 	}
 }
